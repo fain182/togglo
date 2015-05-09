b52656694f5409aae6e51d1b775bf34daa61a8de
fain182 <fain182@gmail.com>
2015-05-09 17:46:15 +0200

    refactored api

--- toggl.go
+++ toggl.go
@@ -4,26 +4,44 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"os"
 )
 
 func sendTimeEntry(entry *TimeEntry) {
-	configuration := getConfiguration()
 	entryJson, err := json.Marshal(entry)
+	if err != nil {
+		println("error: " + err.Error())
+		return
+	}
 	var jsonStr = "{\"time_entry\":" + string(entryJson) + "}"
-	req, err := http.NewRequest("POST", "https://www.toggl.com/api/v8/time_entries", bytes.NewBufferString(jsonStr))
+	sendApiRequest("/time_entries", jsonStr)
+}
+
+func sendApiRequest(togglRelativeUrl, jsonBody string) {
+	configuration := getConfiguration()
+	url := "https://www.toggl.com/api/v8" + togglRelativeUrl
+	req, err := http.NewRequest("POST", url, bytes.NewBufferString(jsonBody))
+	if err != nil {
+		println("error: " + err.Error())
+		return
+	}
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(configuration.ApiToken, "api_token")
 
 	var client = http.Client{}
 	resp, err := client.Do(req)
+
 	if err != nil {
 		println("error: " + err.Error())
 		return
 	}
+
 	if resp.StatusCode != 200 {
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(resp.Body)
 		errorBody := buf.String()
+		println(resp.Status)
 		println(errorBody)
+		os.Exit(1)
 	}
 }
